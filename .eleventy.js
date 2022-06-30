5592ed9b422faba224571316a02234af431710ae
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2022-06-29 18:56:22 -0700

    Fix eleventy.js
    Troubleshooting config.yml issue - added pass through copy

--- .eleventy.js
+++ .eleventy.js
@@ -43,6 +43,7 @@ module.exports = function (eleventyConfig) {
 	}
 
 	eleventyConfig.addPassthroughCopy("./src/assets/");
+	eleventyConfig.addPassthroughCopy("./src/admin");
 
 	// Watch targets
 	eleventyConfig.addWatchTarget("./src/styles/");
