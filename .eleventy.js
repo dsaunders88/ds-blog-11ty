26992b061dfd1632257c5b187f079559bfbc032a
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2022-11-11 15:43:36 -0800

    redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -49,6 +49,7 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy("./src/assets/");
 	eleventyConfig.addPassthroughCopy("./src/admin");
+	eleventyConfig.addPassthroughCopy("./_redirects")
 
 	// Watch targets
 	eleventyConfig.addWatchTarget("./src/styles/");
